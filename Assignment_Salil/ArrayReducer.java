596a9a1f3e3a04574718f787504f421a7239d981
SalilKnols <salil.verma@knoldus.com>
2023-02-17 16:51:00 +0530

    Updated Assignment Code

--- Assignment_Salil/ArrayReducer.java
+++ Assignment_Salil/ArrayReducer.java
@@ -3,53 +3,54 @@ import java.util.Scanner;
 public class ArrayReducer {
 
     // Method to compute the reduced form of an input array
-    public static int[] reduceArray(int[] arr) {
-        int n = arr.length;
+    public static int[] reduceArray(int[] inputArr) {
+        int arrayLength = inputArr.length;
         
         // Create a copy of the array to avoid modifying the original array
-        int[] arrCopy = new int[n];
-        for (int i = 0; i < n; i++) {
-            arrCopy[i] = arr[i];
+        int[] inputArrCopy = new int[arrayLength];
+        for (int inputIndex = 0; inputIndex < arrayLength; inputIndex++) {
+            inputArrCopy[inputIndex] = inputArr[inputIndex];
         }
         
         // Sorted the copy using bubble sort
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arrCopy[j] > arrCopy[j+1]) {
-                    int temp = arrCopy[j];
-                    arrCopy[j] = arrCopy[j+1];
-                    arrCopy[j+1] = temp;
+        for (int outerIndex = 0; outerIndex < arrayLength - 1; outerIndex++) {
+            for (int innerIndex = 0; innerIndex < arrayLength - outerIndex - 1; innerIndex++) {
+                if (inputArrCopy[innerIndex] > inputArrCopy[innerIndex+1]) {
+                    int temp = inputArrCopy[innerIndex];
+                    inputArrCopy[innerIndex] = inputArrCopy[innerIndex+1];
+                    inputArrCopy[innerIndex+1] = temp;
                 }
             }
         }
 
         // Create the reduced form of the input array
-        int[] reducedArr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] reducedArr = new int[arrayLength];
+        for (int reducedIndex = 0; reducedIndex < arrayLength; reducedIndex++) {
             int index = 0;
-            while (arr[i] != arrCopy[index]) {
+            while (inputArr[reducedIndex] != inputArrCopy[index]) {
                 index++;
             }
-            reducedArr[i] = index;
+            reducedArr[reducedIndex] = index;
         }
         return reducedArr;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
         System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        int inputVar = userInput.nextInt();
+        int[] arr = new int[inputVar];
         System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        for (int elementIndex = 0; elementIndex < inputVar; elementIndex++) {
+            arr[elementIndex] = userInput.nextInt();
         }
         
         int[] reducedArr = reduceArray(arr);
         System.out.println("Reduced form of the array: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(reducedArr[i] + " ");
+        for (int reducedElementIndex = 0; reducedElementIndex < inputVar; reducedElementIndex++) {
+            System.out.print(reducedArr[reducedElementIndex] + " ");
         }
     }
 }
+
